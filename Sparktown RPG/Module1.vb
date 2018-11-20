5a20a978f6cf9f7eac19d1db3d6298972632f8d8
NoorA23 <NoorA23@FLMS-X015893>
2018-11-20 14:40:59 -0500

    Add project files.

---
+++ Sparktown RPG/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.ForegroundColor = ConsoleColor.DarkYellow
+        Console.WriteLine("Welcome to SPARKTOWN RPG!")
+        Console.WriteLine("What is your name?")
+        Dim UserName = Console.ReadLine()
+        Console.WriteLine("Welcome, " + UserName)
+        Wait(1.5)
+        Console.WriteLine("Let me tell you the story of the kingdom of SPARKLAND")
+        Wait(1.5)
+    End Sub
+
+    Sub Wait(time)
+        Threading.Thread.Sleep(time * 1000)
+    End Sub
+End Module
---
+++ Sparktown RPG/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Sparktown RPG")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Sparktown RPG")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b78a88ef-50c8-42fe-b3b0-34278d432ded")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
